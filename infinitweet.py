ec88ee6241a9a75ab3d89820e5d4efb7434b2f82
thekindlyone <dodo.dodder@gmail.com>
2015-11-12 23:34:55 +0530

    autopep8

--- infinitweet.py
+++ infinitweet.py
@@ -7,88 +7,90 @@ from PyQt4.QtWebKit import *
 from PyQt4.QtCore import *
 from bs4 import BeautifulSoup as bs
 
+
 class MyDialog(QtGui.QDialog):
+
     def __init__(self, parent=None):
         super(MyDialog, self).__init__(parent)
         self.resize(800, 600)
         self.browser = Browser(self)
 
+
 class Browser(QWebView):
 
-    def __init__(self,parent=None):
-        super(Browser,self).__init__(parent)
-        self.auth_done=False
-        self.auth,auth_url = auth_step1()
+    def __init__(self, parent=None):
+        super(Browser, self).__init__(parent)
+        self.auth_done = False
+        self.auth, auth_url = auth_step1()
         self.loadFinished.connect(self._result_available)
-        self.load(QUrl(auth_url))       
+        self.load(QUrl(auth_url))
 
     def _result_available(self, ok):
         if not self.auth_done:
             frame = self.page().mainFrame()
-            html= unicode(frame.toHtml()).encode('utf-8')
-            soup=bs(html,'lxml')
-            elem=soup.find('code')
+            html = unicode(frame.toHtml()).encode('utf-8')
+            soup = bs(html, 'lxml')
+            elem = soup.find('code')
             if elem:
                 pincode = elem.text
-                twitter = auth_step2(self.auth,pincode)
-                self.auth_done=True
+                twitter = auth_step2(self.auth, pincode)
+                self.auth_done = True
                 self.parent().close()
 
-    
     # def load_userdata(self,twitter):
     #     html = json2html.convert(json = twitter.verify_credentials())
     #     self.setHtml(html)
 
 
-
 class MyWindow(QtGui.QWidget):
+
     def __init__(self, parent=None):
         super(MyWindow, self).__init__(parent)
-        
-        self.textbox=QtGui.QTextEdit(self)
-        self.textbox.textChanged.connect(self.update_char_count) 
+
+        self.textbox = QtGui.QTextEdit(self)
+        self.textbox.textChanged.connect(self.update_char_count)
 
         self.count_display = QtGui.QLabel(self)
         self.count_display.setAlignment(Qt.AlignCenter)
-        
-        self.status_display=QtGui.QLabel(self)
+
+        self.status_display = QtGui.QLabel(self)
         self.status_display.setOpenExternalLinks(True)
-        
-        self.tweetbutton=QtGui.QPushButton('Tweet!',self)
+
+        self.tweetbutton = QtGui.QPushButton('Tweet!', self)
         self.tweetbutton.clicked.connect(self.send_tweet)
-        
+
         self.layout = QtGui.QVBoxLayout(self)
         self.layout.addWidget(self.textbox)
         self.layout.addWidget(self.count_display)
         self.layout.addWidget(self.tweetbutton)
         self.layout.addWidget(self.status_display)
-        self.browser=MyDialog(self)
-        self.twitter=check_stored_tokens()
+        self.browser = MyDialog(self)
+        self.twitter = check_stored_tokens()
         if not self.twitter:
             self.browser.exec_()
-            self.twitter=check_stored_tokens() 
-
+            self.twitter = check_stored_tokens()
 
     @QtCore.pyqtSlot()
     def send_tweet(self):
-        text= str(self.textbox.toPlainText())
-        if len(text)<140:
-            r=self.twitter.update_status(status=text)
+        text = to_bytes(self.textbox.toPlainText())
+        if len(text) < 140:
+            r = self.twitter.update_status(status=text)
         else:
-            status_text=get_tweet_components(text)
-            fn=imagify(text)
-            media=self.twitter.media_upload(fn)
-            r=self.twitter.update_status(status=status_text,media_ids=[media.media_id_string])
+            status_text = get_tweet_components(text)
+            fn = imagify(text)
+            media = self.twitter.media_upload(fn)
+            r = self.twitter.update_status(
+                status=status_text, media_ids=[media.media_id_string])
 
-        template='https://twitter.com/{}/status/{}'.format
-        url='<a href="{}">Tweeted Succesfully</a>'.format(template(r.user.screen_name,r.id_str))
+        template = 'https://twitter.com/{}/status/{}'.format
+        url = '<a href="{}">Tweeted Succesfully</a>'.format(
+            template(r.user.screen_name, r.id_str))
         self.textbox.clear()
         self.status_display.setText(url)
 
     @QtCore.pyqtSlot()
     def update_char_count(self):
         self.count_display.setNum(len(self.textbox.toPlainText()))
-
 
 
 if __name__ == "__main__":
@@ -100,7 +102,7 @@ if __name__ == "__main__":
     app.setApplicationName('Infinitweet')
 
     main = MyWindow()
-    main.resize(width/4,height/3)
+    main.resize(width / 4, height / 3)
     main.show()
 
     sys.exit(app.exec_())
--- utils.py
+++ utils.py
@@ -8,20 +8,23 @@ import textwrap
 from KEYS import *
 from ttp import ttp
 
+
 def auth_step1():
     auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
     auth_url = auth.get_authorization_url()
 
-    return auth,auth_url
+    return auth, auth_url
 
-def auth_step2(auth,pincode):
+
+def auth_step2(auth, pincode):
     auth.get_access_token(pincode)
-    final_token=auth.access_token
-    final_secret=auth.access_token_secret
+    final_token = auth.access_token
+    final_secret = auth.access_token_secret
     auth.set_access_token(final_token, final_secret)
-    twitter=tweepy.API(auth)
-    with open('creds.json','w') as f:
-        json.dump({'oauth_token':final_token,'oauth_token_secret':final_secret},f)
+    twitter = tweepy.API(auth)
+    with open('creds.json', 'w') as f:
+        json.dump(
+            {'oauth_token': final_token, 'oauth_token_secret': final_secret}, f)
     return(twitter)
 
 
@@ -29,12 +32,12 @@ def check_stored_tokens():
     if os.path.exists('creds.json'):
         with open('creds.json') as f:
             try:
-                final_step=json.load(f)
+                final_step = json.load(f)
                 final_token = final_step['oauth_token']
                 final_secret = final_step['oauth_token_secret']
                 auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
                 auth.set_access_token(final_token, final_secret)
-                twitter=tweepy.API(auth)
+                twitter = tweepy.API(auth)
                 return twitter
             except Exception as e:
                 print e
@@ -43,22 +46,24 @@ def check_stored_tokens():
         return None
 
 
-
 def flatten(listOfLists):
     "Flatten one level of nesting"
     return chain.from_iterable(listOfLists)
-    
-def imagify(text,fn='temp.png'):
+
+
+def imagify(text, fn='temp.png'):
     FOREGROUND = (0, 0, 0)
     font_path = 'Times_New_Roman.ttf'
     font = ImageFont.truetype(font_path, 18, encoding='unic')
-    text = to_bytes(text)
-    orilines=text.split('\n')
-    lines = list(flatten([textwrap.wrap(oriline, width=80) if oriline else '    ' for oriline in orilines]))
-    widths,heights=zip(*[font.getsize(line) if line else font.getsize('A') for line in lines ])
-    h=sum(heights)+6*len(lines)+10
-    w=max(widths) +30
-    bg=Image.new('RGBA',(w,h), "#FFFFFF")
+    # text = to_bytes(text)
+    orilines = text.split('\n')
+    lines = list(flatten(
+        [textwrap.wrap(oriline, width=80) if oriline else '    ' for oriline in orilines]))
+    widths, heights = zip(
+        *[font.getsize(line) if line else font.getsize('A') for line in lines])
+    h = sum(heights) + 6 * len(lines) + 10
+    w = max(widths) + 30
+    bg = Image.new('RGBA', (w, h), "#FFFFFF")
     draw = ImageDraw.Draw(bg)
     y_text = 10
     for line in lines:
@@ -67,25 +72,23 @@ def imagify(text,fn='temp.png'):
         else:
             width, height = font.getsize('A')
         draw.text(((w - width) / 2, y_text), line, font=font, fill=FOREGROUND)
-        y_text += height+5        
+        y_text += height + 5
 
-    with open(fn,'w') as f:
+    with open(fn, 'w') as f:
         bg.save(f)
     return fn
 
-parser=ttp.Parser()
+parser = ttp.Parser()
+
+
 def get_tweet_components(text):
-    result=parser.parse(text)
-    urls=' '.join(result.urls)
-    tags=' '.join(['#'+tag for tag in result.tags])
+    result = parser.parse(text)
+    urls = ' '.join(result.urls)
+    tags = ' '.join(['#' + tag for tag in result.tags])
     if result.reply:
         result.users.remove(result.reply)
-        result.reply='@'+result.reply
+        result.reply = '@' + result.reply
     else:
-        result.reply=''
-    users=' '.join(['@'+user for user in result.users])
-    return ' '.join([result.reply,urls,users,tags]).strip()
-
-
-
-
+        result.reply = ''
+    users = ' '.join(['@' + user for user in result.users])
+    return ' '.join([result.reply, urls, users, tags]).strip()
